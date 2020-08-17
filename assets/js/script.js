362a60b7d4ed8f3e4fae3d1f9ce05401966f538b
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-17 15:45:12 -0700

    added JS functionality to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");//finds the <form> element in the page and saves it to the variable formEl
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault(); //stop the browser from automatically reloading the page--default behavior from legacy systems
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
+
+
+
 }
 
 
 
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+
+
+formEl.addEventListener("submit", createTaskHandler);//uses a form specific event called submit (or onsubmit) listening for 2 events within the context of the form--clicking button or pressing enter
 
 
 
