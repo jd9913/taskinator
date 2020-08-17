d0a471fb8e0fddb31e4463eb9bc49579875574fc
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-17 16:28:45 -0700

    code organization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
 var formEl = document.querySelector("#task-form");//finds the <form> element in the page and saves it to the variable formEl
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
 
     event.preventDefault(); //stop the browser from automatically reloading the page--default behavior from legacy systems
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-   
+
+    //package up data as an object
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it an an argument to createTaskEl
+    createTaskEl(taskDataObj);
+
+}
+
+var createTaskEl = function (taskDataObj) {
+
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -18,16 +32,12 @@ var createTaskHandler = function (event) {
     //give it a class name
     taskInfoEl.className = "task-info";
     //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
 
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-
-
-
-
 
 
 }
@@ -38,7 +48,7 @@ var createTaskHandler = function (event) {
 
 
 
-formEl.addEventListener("submit", createTaskHandler);//uses a form specific event called submit (or onsubmit) listening for 2 events within the context of the form--clicking button or pressing enter
+formEl.addEventListener("submit", taskFormHandler);//uses a form specific event called submit (or onsubmit) listening for 2 events within the context of the form--clicking button or pressing enter
 
 
 
